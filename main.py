1b0eb96c184bd22356ff9841ea45a0b676f7124a
BramHamaekers <bram.hamaekers@gmail.com>
2021-10-02 16:13:01 +0200

    bad rank implementation due to time pressure

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 # scripts
 import player_card
 
+
 def get_player_name() -> str:
     print('> Input a player name')
     player_name: str = input('> ')
@@ -25,13 +26,12 @@ def is_valid_request(json: dict) -> bool:
     return json['success']
 
 
-# avatar revision for avatars maybe?
 def get_card_data(json: dict):
     user: dict = json['data'].get('user')
     league: dict = user.get('league')
 
     username: str = user.get('username')
-    id: str = user.get('_id')
+    _id: str = user.get('_id')
     avatar_id: str = user.get('avatar_revision')
     country: str = user.get('country')
     rank: str = league.get('rank')
@@ -40,10 +40,36 @@ def get_card_data(json: dict):
     apm: int = league.get('apm')
     vs: int = league.get('vs')
 
-    player_card_data: dict = {'username': username, 'id': id, 'avatar': avatar_id, 'country': country, 'rank': rank,
+    player_card_data: dict = {'username': username, '_id': _id, 'avatar': avatar_id, 'country': country, 'rank': rank,
                               'tr': tr, 'pps': pps, 'apm': apm, 'vs': vs}
-
     return player_card_data
+
+
+def get_avatar_url(data):
+    avatar_id: str = str(data.get('avatar'))
+    if not is_valid_avatar_id(avatar_id):
+        return None
+    default: str = 'https://tetr.io/user-content/avatars/'
+    _id: str = data.get('_id')
+    jpg: str = '.jpg?rv='
+
+    url = ''.join([default, _id, jpg, avatar_id])
+    return url
+
+
+def is_valid_avatar_id(_id: str) -> bool:
+    if _id != 'None':
+        return True
+    return False
+
+
+def get_avatar(url):
+    if url is None:
+        return
+    r = requests.get(url)
+    file = open("avatar.jpg", "wb")
+    file.write(r.content)
+    file.close()
 
 
 def main():
@@ -57,7 +83,12 @@ def main():
         return
 
     data = get_card_data(p_json)
-    player_card.render(data)
+    avatar_url = get_avatar_url(data)
+    get_avatar(avatar_url)
+    player_card.render_text(data)
+    player_card.render_avatar()
+
+    main()
 
 
 if __name__ == '__main__':
--- player_card.py
+++ player_card.py
@@ -1,13 +1,16 @@
 from PIL import Image, ImageFont, ImageDraw
+import os
 
 
-def render(data: dict):
-    img = Image.open("background.png")
+def render_text(data: dict):
+    img = Image.open("background.jpg")
     draw = ImageDraw.Draw(img)
 
     # Set user and data fonts
     user_font = ImageFont.truetype(font="bahnschrift.ttf", size=144)
     user_font.set_variation_by_name('Bold SemiCondensed')
+    country_font = ImageFont.truetype(font="bahnschrift.ttf", size=80)
+    country_font.set_variation_by_name('Bold SemiCondensed')
     data_font = ImageFont.truetype(font="bahnschrift.ttf", size=108)
     data_font.set_variation_by_name('Bold SemiCondensed')
 
@@ -24,6 +27,12 @@ def render(data: dict):
     draw.text((center, top), text, (255, 255, 255), font=user_font, anchor="ma")
     draw.text((center - 7, top - 7), text, (255, 195, 30), font=user_font, anchor="ma")
 
+    # Temporary draw country
+    text = data.get('country')
+    print(text)
+    text = text.upper()
+    draw.text((center, top + 150), text, (255, 255, 255), font=country_font, anchor="ma")
+
     # Draw data
     data_elements = ['tr', 'pps', 'apm', 'vs']
     for elem in data_elements:
@@ -33,4 +42,27 @@ def render(data: dict):
         draw.text((right, data_top), text, (255, 255, 255), font=data_font, anchor="ra")
         data_top += 150
 
-    img.save("text.png")
+    img.convert('RGB').save('card.jpg')
+    render_rank(data.get('rank'))
+
+
+def render_rank(rank):
+    img = Image.open('card.jpg')
+    rank = Image.open('rank_gfx/' + rank + '.jpg')
+    img.paste(rank, (1920-485, 85))
+    img.save('card.jpg')
+
+
+def render_avatar():
+    img = Image.open('card.jpg')
+    try:
+        avatar = Image.open('avatar.jpg')
+    except FileNotFoundError:
+        avatar = Image.open('default_avatar.jpg')
+    img.paste(avatar, (185, 85))
+    img.save('card.jpg')
+    img.show()
+    try:
+        os.remove('avatar.jpg')
+    except FileNotFoundError:
+        return
